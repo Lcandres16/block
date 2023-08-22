7b5d8e5a09abcc691b7d4e0757347865ee16a9a5
Lcandres16 <palucero@sudamericano.edu.ec>
2023-08-21 23:57:20 -0500

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const exploreButton = document.getElementById('exploreButton');
+
+    exploreButton.addEventListener('click', function() {
+        // Aquí podrías redirigir a otra página o realizar alguna otra acción al hacer clic en el botón "Explorar".
+        alert('¡Has hecho clic en Explorar!');
+    });
+});
